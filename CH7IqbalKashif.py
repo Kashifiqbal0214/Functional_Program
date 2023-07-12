e2a6431d2192e5047208651e3b00a24cae3d2436
kashif <kashifiqbal0214@gmail.com>
2023-07-12 18:06:40 +0500

    Change places fo block comment to under the function

--- CH7IqbalKashif.py
+++ CH7IqbalKashif.py
@@ -8,12 +8,12 @@ TITLE = "Welcom to Number Analysis Program!\n"
 LINE = "-" * len(TITLE)
 LIST_SIZE = 5
 
-"""
-main Function description goes here...
-Including the all function in this function for calling """
-
 
 def main():
+    """
+    main Function description goes here...
+    Including the all function in this function for calling
+    """
     print(TITLE + LINE)
     numsList = getNums()
     lo = findLo(numsList)
@@ -23,12 +23,12 @@ def main():
     displayData(numsList, lo, hi, total, avg)
 
 
-""" This function Describes.. 
-get numbers from user and append the number input to numsList through for loop,
-and return a List """
-
-
 def getNums():
+    """
+    This function Describes..
+    get numbers from user and append the number input to numsList through for loop,
+    and return a List
+    """
     numsList = []
     # For loop.. Collect Integer numbers from user which range is 5 List_size variable
     for nums in range(LIST_SIZE):
@@ -37,14 +37,14 @@ def getNums():
     return numsList
 
 
-"""This is function find a Lowest number, 
-use a for loop iterating through  every element in numsList, 
-Use if to check if the element is less then to  lo variable,
-then assign the list element to lo variable, 
-and finally a return lowest number in lo variable"""
-
-
 def findLo(numsList):
+    """
+        This is function find a Lowest number,
+    use a for loop iterating through  every element in numsList,
+    Use if to check if the element is less then to  lo variable,
+    then assign the list element to lo variable,
+    and finally a return lowest number in lo variable
+    """
     lo = numsList[0]
     for num in numsList:
         if num < lo:
@@ -52,12 +52,13 @@ def findLo(numsList):
     return lo
 
 
-""" findHi function  same as  work of the above function findLo but this find a Highest numbers in the list,
-using a if to check if element is greater 
-then hi variable then assign the list element to hi variable"""
-
-
 def findHi(numList):
+    """
+    findHi function  same as  work of the above function
+    findLo but this find a Highest     numbers in the list,
+    using a if to check if element is greater
+    then hi variable then assign the list element to hi variable
+    """
     hi = numList[0]
     for num in numList:
         if num > hi:
@@ -65,28 +66,31 @@ def findHi(numList):
     return hi
 
 
-"""findTotal describes ,  
-all element sums of numslist  through for loop then assign total 
-variable and finally return total vairable"""
-
-
 def findTotal(numsList):
+    """
+    findTotal describes ,
+    all element sums of numslist  through for loop then assign total
+    variable and finally return total vairable
+    """
     total = 0
     for num in numsList:
         total += num
     return total
 
 
-"""findAvg  describes.. how many  Average is total numbers in list"""
-
-
 def findAvg(total):
+    """
+    findAvg  describes.. how many  Average is total numbers in list
+    """
+
     avg = total / LIST_SIZE
     return "{:.2f}".format(avg)
 
 
-# This function displays the data..
 def displayData(numsList, lo, hi, total, avg):
+    """
+    This function displays the data..
+    """
     print(LINE)
     print("Your Input Data:", numsList)
     print("Lowest Numbers: ", lo)
